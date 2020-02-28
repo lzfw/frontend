2ba575ef897024dfba1da029c1fef8d823ce8d6f
Felix Pahlow <felix@wohlpa.de>
2020-02-28 21:52:47 +0100

    leo-cmd.sh: Prevent multi-substitution
    when container restarts

--- leo-cmd.sh
+++ leo-cmd.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # Change the log format for nginx to JSON and send messages through syslog;
-sed -E -i 's/(access_log\s*.+\s*vhost\s*;)/\1 \naccess_log syslog:server=graylog:5555 syslog_json if=$external;/gi' /app/nginx.tmpl
+sed -E -i 's/(access_log\s*.+\s*vhost);/\1 ; \naccess_log syslog:server=graylog:5555 syslog_json if=$external;/gi' /app/nginx.tmpl
 
 exec "$@"
 
